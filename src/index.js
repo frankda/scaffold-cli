b5b4aabf00408cee64d163710deb4d20a55d97c2
frankda <suyunyi@gmail.com>
2021-06-17 21:09:28 +1000

    - Create files according to template

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+import fs from 'fs';
+
+const category = "test";
+const componentName = "rock";
+
+// TODO:
+// iterate template folder and generate file according to file name
+const component = fs.readFileSync('./src/template/component.max', 'utf8');
+const componentTemplate = fs.readFileSync('./src/template/component-template.max', 'utf8');
+const componentScss = fs.readFileSync('./src/template/_component.max', 'utf8');
+
+// TODO:
+// find a way like template engine to replace component inside curly bracket: {{componnet}}
+fs.appendFileSync(componentName + '.js', component.replace('{{component}}', componentName));
+fs.appendFileSync(componentName + '-template.js', componentTemplate.replace('{{component}}', componentName));
+fs.appendFileSync('_' + componentName + '.scss', componentScss);
